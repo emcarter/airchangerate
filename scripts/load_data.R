5b9d4efd87daf917f068e2635df70af98150e1cc
Kelsey  Bilsback <shadowbaby@Kelseys-MacBook.local.tld>
2018-06-12 07:51:12 -0600

    fix script to get rid of lists

--- scripts/load_data.R
+++ scripts/load_data.R
@@ -10,47 +10,50 @@ library(lubridate)
 # file <- "ABU-02-11-VSARBL-1650_PAC7000_29_9_2012.txt"
 # file <- "SUK-02-12ARBL-1596_PAC7000_12_7_2012.txt"
 load_beia_gambia <- function(){
-data <-
-  lapply(list.files("../data/beia-gambia/",
-                    pattern = ".txt",
-                    full.names = TRUE),
+  
+  data <-
+    lapply(list.files("../data/beia-gambia/",
+                      pattern = "[0-9].txt",
+                      full.names = TRUE),
 
-         function(file)
+           function(file)
 
-         # check if file is tab or semicolon delimited
-         if (grepl(";", readr::read_lines(file, n_max = 1))) {
+             # check if file is tab or semicolon delimited
+             if (grepl(";", readr::read_lines(file, n_max = 1))) {
 
-          # read file with semicolon delimiter
-          readr::read_delim(file,
-                            delim = ";",
-                            skip = 33,
-                            col_types =
-                              cols(x1 = col_character(),
-                                   datetime = col_character(),
-                                   val = col_double()),
-                            col_names = c("x1", "datetime", "val"),
-                            na = c("", "NA")) %>%
-          dplyr::mutate(logger_id = sub(".*-", "", file),
-                        logger_id = gsub("_.*", "", logger_id))
+               # read file with semicolon delimiter
+               readr::read_delim(file,
+                                 delim = ";",
+                                 skip = 33,
+                                 col_types =
+                                   cols(x1 = col_character(),
+                                        datetime = col_character(),
+                                        val = col_double()),
+                                 col_names = c("x1", "datetime", "val"),
+                                 na = c("", "NA")) %>%
+               dplyr::mutate(logger_id = sub(".*-", "", file),
+                             logger_id = gsub("_.*", "", logger_id))
 
-         }else{
+             }else{
 
-           # read file with tab delimiter
-           readr::read_tsv(file,
-                           skip = 33,
-                           col_types =
-                             cols(x1 = col_character(),
-                                  datetime = col_character(),
-                                  val = col_double()),
-                           col_names = c("x1", "datetime", "val"),
-                           na = c("", "NA")) %>%
-             dplyr::mutate(logger_id = sub(".*-", "", file),
-                           logger_id = gsub("_.*", "", logger_id))
-           }
-        )
+               # read file with tab delimiter
+               readr::read_tsv(file,
+                               skip = 33,
+                               col_types =
+                                 cols(x1 = col_character(),
+                                      datetime = col_character(),
+                                      val = col_double()),
+                               col_names = c("x1", "datetime", "val"),
+                               na = c("", "NA")) %>%
+                 dplyr::mutate(logger_id = sub(".*-", "", file),
+                               logger_id = gsub("_.*", "", logger_id))
+               }
+           )
 
-    dplyr::bind_rows() %>%
-    dplyr::select(-x1) %>%
-    dplyr::mutate(pol = "co")
+  data <- 
+    data %>%
+      do.call(rbind.data.frame, .) %>%
+      dplyr::select(-x1) %>%
+      dplyr::mutate(pol = "co")
 }
 #________________________________________________________
